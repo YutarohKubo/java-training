b2cae824083a7b5c25e4e74bf5e7c62a90841450
Yutaroh Kubo <yuutaroh.kubo@nts.ricoh.co.jp>
2019-10-11 22:39:29 +0900

    ch03ex11 is done

--- jpl/src/ch03/ex11/SimpleSortDouble.java
+++ jpl/src/ch03/ex11/SimpleSortDouble.java
@@ -4,7 +4,11 @@ public class SimpleSortDouble extends SortDouble{
     @Override
     protected void doSort() {
         for (int i = 0; i < getDataLength(); i++) {
-
+            for (int j = i + 1; j < getDataLength(); j++) {
+                if (compare(i, j) > 0) {
+                    swap(i, j);
+                }
+            }
         }
     }
 
--- jpl/src/ch03/ex11/SortDouble.java
+++ jpl/src/ch03/ex11/SortDouble.java
@@ -20,6 +20,28 @@ public abstract class SortDouble {
         return values.length;
     }
 
+    protected final double probe(int i) {
+        curMetrics.probeCnt++;
+        return values[i];
+    }
+
+    protected final int compare(int i, int j) {
+        curMetrics.compareCnt++;
+        double d1 = values[i];
+        double d2 = values[j];
+        if (d1 == d2) {
+            return 0;
+        } else {
+            return (d1 < d2 ? -1 : 1);
+        }
+    }
+
+    protected final void swap(int i, int j) {
+        curMetrics.swapCnt++;
+        double tmp = values[i];
+        values[i] = values[j];
+        values[j] = tmp;
+    }
 
     protected abstract void doSort();
 
