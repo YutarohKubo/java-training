d173028a34820cf5a7ca9b9f6ae2b905aa6b21d5
Yutaroh Kubo <channyuchannyu@gmail.com>
2020-02-12 09:29:46 +0900

    ch17/ex01 is done

--- jpl/src/ch17/ex01/Main.java
+++ jpl/src/ch17/ex01/Main.java
@@ -3,7 +3,17 @@ package ch17.ex01;
 public class Main {
 
     public static void main(String[] args) {
-
+        Runtime rt = Runtime.getRuntime();
+        long freeMemory1 = rt.freeMemory();
+        System.out.println("free memory when app launch = " + freeMemory1);
+        for (int i = 0; i < 10000; i++) {
+            String str = new String("hogehogehogehoge");
+        }
+        long freeMemory2 = rt.freeMemory();
+        System.out.println("free memory after created many String = " + freeMemory2);
+        rt.gc();
+        long freeMemory3 = rt.freeMemory();
+        System.out.println("free memory after gc = " + freeMemory3);
     }
 
 }
